dc5330a5ff69622cc3abe5e1a6cc31a259c2c2fc
MarcusGrass <marcus.grass@protonmail.com>
2024-03-22 10:10:11 +0100

    Map right keys

--- rp2040-kbd/src/keyboard.rs
+++ rp2040-kbd/src/keyboard.rs
@@ -1,7 +1,5 @@
 use embedded_hal::digital::v2::{InputPin, OutputPin, PinState};
-use rp2040_hal::gpio::bank0::{
-    Gpio20, Gpio21, Gpio22, Gpio23, Gpio26, Gpio27, Gpio29, Gpio6, Gpio7, Gpio8, Gpio9,
-};
+use rp2040_hal::gpio::bank0::{Gpio20, Gpio21, Gpio22, Gpio23, Gpio26, Gpio27, Gpio29, Gpio4, Gpio5, Gpio6, Gpio7, Gpio8, Gpio9};
 use rp2040_hal::gpio::{DynPinId, FunctionSio, Pin, PinId, PullUp, SioInput};
 
 type RowPin = Pin<DynPinId, FunctionSio<SioInput>, PullUp>;
@@ -43,21 +41,6 @@ pub struct ButtonStateChange {
     new_state: ButtonState,
 }
 
-pub struct Left {
-    prev_matrix: MatrixState,
-    rows: [
-        RowPin; 5
-    ],
-    cols: (
-        Option<ButtonPin<Gpio9>>,
-        Option<ButtonPin<Gpio26>>,
-        Option<ButtonPin<Gpio22>>,
-        Option<ButtonPin<Gpio20>>,
-        Option<ButtonPin<Gpio23>>,
-        Option<ButtonPin<Gpio21>>,
-    ),
-}
-
 pub type MatrixState = [[ButtonState; NUM_COLS]; NUM_ROWS];
 
 const INITIAL_STATE: MatrixState = [[ButtonState::Depressed; NUM_COLS]; NUM_ROWS];
@@ -88,6 +71,22 @@ macro_rules! check_col {
 
     };
 }
+
+pub struct Left {
+    prev_matrix: MatrixState,
+    rows: [
+        RowPin; 5
+    ],
+    cols: (
+        Option<ButtonPin<Gpio9>>,
+        Option<ButtonPin<Gpio26>>,
+        Option<ButtonPin<Gpio22>>,
+        Option<ButtonPin<Gpio20>>,
+        Option<ButtonPin<Gpio23>>,
+        Option<ButtonPin<Gpio21>>,
+    ),
+}
+
 impl Left {
     pub fn new(
         rows: (
@@ -121,6 +120,63 @@ impl Left {
         check_col!(self, 2, next_state, changes);
         check_col!(self, 3, next_state, changes);
         check_col!(self, 4, next_state, changes);
+        // Todo: Row 4 gets weird, may be because it has fewer buttons, may be wrongly mapped
+        check_col!(self, 5, next_state, changes);
+        self.prev_matrix = next_state;
+        changes
+    }
+
+}
+
+pub struct Right {
+    prev_matrix: MatrixState,
+    rows: [
+        RowPin; 5
+    ],
+    cols: (
+        Option<ButtonPin<Gpio9>>,
+        Option<ButtonPin<Gpio26>>,
+        Option<ButtonPin<Gpio22>>,
+        Option<ButtonPin<Gpio20>>,
+        Option<ButtonPin<Gpio23>>,
+        Option<ButtonPin<Gpio21>>,
+    ),
+}
+
+impl Right {
+    pub fn new(
+        rows: (
+            ButtonPin<Gpio29>,
+            ButtonPin<Gpio4>,
+            ButtonPin<Gpio20>,
+            ButtonPin<Gpio23>,
+            ButtonPin<Gpio21>,
+        ),
+        cols: (
+            Option<ButtonPin<Gpio9>>,
+            Option<ButtonPin<Gpio26>>,
+            Option<ButtonPin<Gpio22>>,
+            Option<ButtonPin<Gpio20>>,
+            Option<ButtonPin<Gpio23>>,
+            Option<ButtonPin<Gpio21>>,
+        ),
+    ) -> Self {
+        Self {
+            prev_matrix: INITIAL_STATE,
+            rows: [rows.0.into_dyn_pin(), rows.1.into_dyn_pin(), rows.2.into_dyn_pin(), rows.3.into_dyn_pin(), rows.4.into_dyn_pin()],
+            cols,
+        }
+    }
+
+    pub fn scan_matrix(&mut self) -> heapless::Vec<ButtonStateChange, 16> {
+        let mut next_state = INITIAL_STATE;
+        let mut changes = heapless::Vec::new();
+        check_col!(self, 0, next_state, changes);
+        check_col!(self, 1, next_state, changes);
+        check_col!(self, 2, next_state, changes);
+        check_col!(self, 3, next_state, changes);
+        check_col!(self, 4, next_state, changes);
+        // Todo: Row 4 gets weird, may be because it has fewer buttons, may be wrongly mapped
         check_col!(self, 5, next_state, changes);
         self.prev_matrix = next_state;
         changes
